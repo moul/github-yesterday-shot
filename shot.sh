1d8e70078e4f2e936f8f3525f69deb5e992ed140
Manfred Touron <94029+moul@users.noreply.github.com>
2020-05-14 02:53:25 +0200

    fix: support osx

--- shot.sh
+++ shot.sh
@@ -2,7 +2,7 @@
 
 username=${1:-$USER}
 when=${2:-1 day ago}
-date=$(date --date="$when" "+%Y-%m-%d")
+date=$(date --date="$when" "+%Y-%m-%d" 2>/dev/null || gdate --date="$when" "+%Y-%m-%d")
 
 # remove previous shot if existing
 rm -f shot.png
