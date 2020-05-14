255ebf31cbe9cb2b301429ee81ea5463b6d017e9
Manfred Touron <94029+moul@users.noreply.github.com>
2020-05-14 02:50:40 +0200

    feat: v1

---
+++ shot.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+username=${1:-$USER}
+when=${2:-1 day ago}
+date=$(date --date="$when" "+%Y-%m-%d")
+
+# remove previous shot if existing
+rm -f shot.png
+
+# take the shot
+pageres \
+     "https://github.com/${username}?tab=overview&from=${date}&to=${date}" \
+     --selector=.profile-timeline \
+     --css=".profile-timeline{padding:20px;border 0;.profile-timeline.discussion-timeline::before{background:transparent}" \
+     --filename=shot
+
+# result
+ls -la shot.png
