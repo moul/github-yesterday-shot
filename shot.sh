02f04e94e5fc75f4bdfdadbd19d0428e42ac12e2
Manfred Touron <94029+moul@users.noreply.github.com>
2021-04-29 17:47:55 +0200

    chore: try to use capture-website

--- shot.sh
+++ shot.sh
@@ -8,13 +8,26 @@ date=$(date --date="$when" "+%Y-%m-%d" 2>/dev/null || gdate --date="$when" "+%Y-
 rm -f shot.png
 
 # take the shot
+#capture-website \
+#    "https://github.com/${username}?tab=overview&from=${date}&to=${date}" \
+#    --verbose \
+#    --timeout=60 \
+#    --overwrite \
+#    --no-javascript \
+#    --disable-animations \
+#    --full-page \
+#    --style="html{background:green}body{background:blue}.application-main{background:yellow}main{background:orange}.contribution-activity-listing{padding:20px;border 0}.contribution-activity-listing.discussion-timeline::before{background:transparent}.profile-rollup-content{display:block}.Box,.js-pinned-items-reorder-container,div[itemtype=\"http://schema.org/Person\"]{display:none}" \
+#    --output=shot.png
+#--emulate-device="iPhone X" \
+
 pageres \
-     "https://github.com/${username}?tab=overview&from=${date}&to=${date}" \
-     --verbose \
-     --selector=.contribution-activity-listing \
-     --css=".contribution-activity-listing{padding:20px;border 0}.contribution-activity-listing.discussion-timeline::before{background:transparent}.profile-rollup-content{display:block}" \
-     --overwrite \
-     --filename=shot
+    "https://github.com/${username}?tab=overview&from=${date}&to=${date}" \
+    --delay=5 \
+    --verbose \
+    --overwrite \
+    --selector=".contribution-activity-listing" \
+    --css=".contribution-activity-listing{padding:20px;border 0}.contribution-activity-listing.discussion-timeline::before{background:transparent}.profile-rollup-content{display:block}" \
+    --filename=shot
 
 # result
 ls -la shot.png
